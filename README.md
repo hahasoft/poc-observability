3dad6ce26c360196976751b8904981e3b6b09304
hahaman <ghahaman007+soft@gmail.com>
2024-08-16 11:47:53 +0700

    lab7: micrometer @Timed

--- README.md
+++ README.md
@@ -17,6 +17,8 @@
 - http://localhost:8080/actuator/metrics/
 - http://localhost:8080/actuator/metrics/jvm.memory.used
 - http://localhost:8080/actuator/metrics/jvm.memory.used?tag=area:heap
+- http://localhost:8080/actuator/metrics/do.sleep.method.timed
+- http://localhost:8080/actuator/metrics/http.server.requests?tag=uri:/sleep
 
 ## Ref
 - https://grafana.com/docs/tempo/latest/getting-started/
--- src/main/java/com/hahasoft/poc/observability/PocObservabilityApplication.java
+++ src/main/java/com/hahasoft/poc/observability/PocObservabilityApplication.java
@@ -1,5 +1,8 @@
 package com.hahasoft.poc.observability;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
@@ -11,9 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @Slf4j
@@ -27,15 +34,20 @@ public class PocObservabilityApplication {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
-
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
+	}
 
 	@RestController
 	class HelloController {
 
 		private final RestTemplate restTemplate;
+		private final SleepService sleepService;
 
-		HelloController(RestTemplate restTemplate) {
+		HelloController(RestTemplate restTemplate, SleepService sleepService) {
 			this.restTemplate = restTemplate;
+			this.sleepService = sleepService;
 		}
 
 		@GetMapping("/hello")
@@ -44,6 +56,30 @@ public class PocObservabilityApplication {
 			MDC.put("test", ""+ System.currentTimeMillis());
 			log.info("response: {}", responseEntity.getBody());
 			return responseEntity.getBody();
+		}
+
+		@GetMapping("/exception")
+		public String exception() {
+			throw new IllegalArgumentException("This id is invalid");
+		}
+
+		@GetMapping("/sleep")
+		public Long sleep(@RequestParam Long ms) {
+			Long result = this.sleepService.doSleep(ms);
+			return result;
+		}
+	}
+
+	@Service
+	class SleepService {
+		@Timed(value = "do.sleep.method.timed")
+		public Long doSleep(Long ms) {
+			try {
+				TimeUnit.MILLISECONDS.sleep(ms);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			return ms;
 		}
 	}
 
